f82f4a4c5d2e35ac7c0912b689251c8695c4d4ad
Martin Grd <martin-guyard@live.fr>
2020-11-27 01:10:59 +0100

    Prework

--- beacon/app/index.ts
+++ beacon/app/index.ts
@@ -1,11 +1,50 @@
-import http from 'http';
+/* eslint-disable no-console */
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
-const server = http.createServer().listen(3000, '0.0.0.0');
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const io = require('socket.io')(server);
+const httpServer = createServer().listen(3000, '0.0.0.0');
 
-// eslint-disable-next-line space-before-function-paren
-io.on('connection', (socket: any) => {
-  // console.log('user connected');
-  socket.emit('welcome', 'welcome man');
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+  },
 });
+
+let cpt = 0;
+
+io.use((socket, next) => {
+  const token = socket.handshake.auth;
+  // ...
+  console.log('AuthnToken: ', token);
+  next();
+});
+
+io.on('connection', (socket: Socket) => {
+  socket.emit('welcome', 'welcome man');
+
+  console.log('SocketId: ', socket.id);
+  console.log('SocketHandshake: ', socket.handshake);
+
+  socket.on('message', () => {
+    console.log('message');
+    cpt += 1;
+    socket.emit('hello', cpt);
+  });
+});
+
+httpServer.listen(3000);
+
+// // in a middleware
+// io.use(async (socket, next) => {
+//   try {
+//     const user = await fetchUser(socket);
+//     socket.user = user;
+//   } catch (e) {
+//     next(new Error("unknown user"));
+//   }
+// });
+
+// const server = require('https').createServer({
+//   key: fs.readFileSync('/tmp/key.pem'),
+//   cert: fs.readFileSync('/tmp/cert.pem')
+// });
