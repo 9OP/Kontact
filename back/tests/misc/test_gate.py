3b0d9576d9259358892dbeb671c5cd6d70de1ba2
Martin Grd <martin-guyard@live.fr>
2021-03-23 02:11:42 +0100

    Testing access, role and authentication gate

--- back/tests/misc/test_gate.py
+++ back/tests/misc/test_gate.py
@@ -1,10 +1,11 @@
 import pytest
 import app.api_responses as api_res
+from app.models import Access, Role
 from app.api.middlewares import gate
 from flask import session, g
 
 
-class TestSuiteAuthentication:
+class TestGateAuthentication:
     @pytest.fixture
     def no_token_context(self, app):
         with app.test_request_context():
@@ -68,7 +69,7 @@ class TestSuiteAuthentication:
         WHEN token set and session set
         THEN ok
         """
-        _, user, _ = token_context
+        _, user, token = token_context
         session["user_id"] = user.id
 
         @gate()
@@ -76,6 +77,8 @@ class TestSuiteAuthentication:
             return "ok"
 
         assert fn() == "ok"
+        assert g.current_user == user
+        assert g.auth_token == token
 
     def test_gate_delegation(self, token_context):
         """
@@ -83,9 +86,154 @@ class TestSuiteAuthentication:
         WHEN token set
         THEN ok
         """
+        _, user, token = token_context
 
         @gate(delegation=True)
         def fn():
             return "ok"
 
         assert fn() == "ok"
+        assert g.current_user == user
+        assert g.auth_token == token
+
+
+class TestGateAccess:
+    @pytest.fixture
+    def context(self, app, _token):
+        token, user = _token
+        with app.test_request_context(
+            headers={"Authorization": f"Bearer {token.encode()}"}
+        ):
+            session["user_id"] = user.id
+            yield app, user, token
+
+    def test_fail_access(self, context):
+        """
+        GIVEN gate access control
+        WHEN user access is low
+        THEN raise AccessError
+        """
+
+        @gate(access=Access.ADMIN)
+        def fn():
+            return "ok"
+
+        with pytest.raises(api_res.AccessError):
+            fn()
+
+    def test_gate_access_user(self, context):
+        """
+        GIVEN gate access control
+        WHEN user access is right
+        THEN ok
+        """
+
+        @gate(access=Access.USER)
+        def fn():
+            return "ok"
+
+        assert fn() == "ok"
+
+    def test_gate_access_admin(self, context):
+        """
+        GIVEN gate and access control
+        WHEN user access is elevated
+        THEN ok
+        """
+        _, user, _ = context
+        user.update(access=Access.ADMIN)
+
+        @gate(access=Access.ADMIN)
+        def fn():
+            return "ok"
+
+        assert fn() == "ok"
+
+
+class TestGateRole:
+    @pytest.fixture
+    def data(self, _user, _channel, make_membership):
+        user, _ = _user
+        channel, _ = _channel
+        return user, channel
+
+    @pytest.fixture
+    def context(self, app, data, make_token, make_membership):
+        user, channel = data
+        membership = make_membership(user_id=user.id, channel_id=channel.id)
+        token = make_token(user_id=user.id)
+        with app.test_request_context(
+            headers={"Authorization": f"Bearer {token.encode()}"}
+        ):
+            session["user_id"] = user.id
+            yield user, channel, membership
+
+    @pytest.fixture
+    def no_membership_context(self, app, data, make_token):
+        user, channel = data
+        token = make_token(user_id=user.id)
+        with app.test_request_context(
+            headers={"Authorization": f"Bearer {token.encode()}"}
+        ):
+            session["user_id"] = user.id
+            yield user, channel
+
+    def test_fail_role(self, context):
+        """
+        GIVEN gate and role control
+        WHEN user role is low
+        THEN raise AccessError
+        """
+        _, channel, _ = context
+
+        @gate(role=Role.MASTER)
+        def fn(cid):
+            return "ok"
+
+        with pytest.raises(api_res.AccessError):
+            fn(cid=channel.id)
+
+    def test_admin_granted(self, context):
+        """
+        GIVEN gate and role control
+        WHEN user access is admin
+        THEN ok
+        """
+        user, channel, _ = context
+        user.update(access=Access.ADMIN)
+
+        @gate(role=Role.MASTER)
+        def fn(cid):
+            return "ok"
+
+        assert fn(cid=channel.id) == "ok"
+
+    def test_fail_no_membership(self, no_membership_context):
+        """
+        GIVEN gate with role control
+        WHEN user is not a member
+        THEN raise AccessError
+        """
+        _, channel = no_membership_context
+
+        @gate(role=Role.MEMBER)
+        def fn(cid):
+            return "ok"
+
+        with pytest.raises(api_res.AccessError):
+            fn(cid=channel.id)
+
+    def test_role_elevated(self, context):
+        """
+        GIVEN gate with role control
+        WHEN user role is elevated
+        THEN ok
+        """
+        _, channel, membership = context
+        membership.update(role=Role.MASTER)
+
+        @gate(role=Role.MASTER)
+        def fn(cid):
+            return "ok"
+
+        assert fn(cid=channel.id) == "ok"
