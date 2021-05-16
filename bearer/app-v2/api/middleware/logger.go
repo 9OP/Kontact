9f6bd346b264b81e904bd133e635ef674c0ce157
Martin <martin@cardiologs.com>
2021-05-16 22:36:19 +0200

    Logger stdout + file

--- bearer/app-v2/api/middleware/logger.go
+++ bearer/app-v2/api/middleware/logger.go
@@ -1,9 +1,14 @@
 package middleware
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
 )
 
 // custom response writer that catch the status code
@@ -25,6 +30,18 @@ func (w *logResponseWriter) WriteHeader(code int) {
 
 func Logger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// file path: logs/api_YYYYMMDD.log
+		path, _ := filepath.Abs(fmt.Sprintf("logs/api_%v.log", time.Now().Format("20060102")))
+
+		f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		defer f.Close()
+
+		wrt := io.MultiWriter(os.Stdout, f)
+		log.SetOutput(wrt)
+
 		logRespWriter := newLogResponseWriter(w)
 
 		// Wrap the input handler in anonymous function call
