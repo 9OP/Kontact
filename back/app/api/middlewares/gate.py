047e67ca1a924b27ca0a2d9f30d1006627c51e49
Martin <martin@cardiologs.com>
2021-08-09 01:00:49 +0200

    rename and clean

--- back/app/api/middlewares/gate.py
+++ back/app/api/middlewares/gate.py
@@ -5,21 +5,20 @@ from .authentication import authentication
 from .authorization import role_control, access_control
 
 
-def validate(**kwargs):
+def validate_url_params(**kwargs):
     cid = kwargs.get("cid", None)
     uid = kwargs.get("uid", None)
 
-    def validUUID(uuid_to_test):
+    def validate_uuid(uuid_to_test):
         try:
-            uuid_obj = UUID(uuid_to_test, version=4)
+            str(UUID(uuid_to_test, version=4)) == uuid_to_test
         except ValueError:
             return False
-        return str(uuid_obj) == uuid_to_test
 
-    if cid and not validUUID(cid):
+    if cid and not validate_uuid(cid):
         raise apr.InvalidParameter("cid is not a UUID-4")
 
-    if uid and not validUUID(uid):
+    if uid and not validate_uuid(uid):
         raise apr.InvalidParameter("uid is not a UUID-4")
 
 
@@ -27,7 +26,7 @@ def gate(**gkwargs):
     def decorator(func):
         @functools.wraps(func)
         def secure_function(*args, **kwargs):
-            validate(**kwargs)
+            validate_url_params(**kwargs)
             authentication(delegation=gkwargs.get("delegation"))
 
             control_switcher = {
